95cfc0c6e9fc641b5265bf7a75fac038641e83fa
Mahesh <Mahesh@minorks.com>
2023-07-31 16:10:28 +0530

    mahesh cms policy upolad

--- internal system/Front End/src/app/cms-module/policy-upload-admin/policy-upload-admin.component.ts
+++ internal system/Front End/src/app/cms-module/policy-upload-admin/policy-upload-admin.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-policy-upload-admin',
@@ -8,15 +10,56 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class PolicyUploadAdminComponent {
 
-Policy_Upload_Admin=new FormGroup<any>({
-  policyTag:new FormControl(''),
-  documentName:new FormControl(' '),
-  uploadDocumet:new FormControl(' '),
-})
 
-policyUploadAdmin(item:any){
-  
-  console.log(item);
 
+
+
+uploadForm: FormGroup;
+
+
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+   
+  ) {
+    this.uploadForm = this.formBuilder.group({
+      policyTag:new FormControl(''),
+      docName:new FormControl(' '),
+      uploadDoc:new FormControl(' '),
+    });
+
+  }
+
+  onSubmit(): void {
+    if (this.uploadForm.valid) {
+      console.log('Form values:', this.uploadForm.value);
+      // Send the form data to the backend POST API
+      const formData = new FormData();
+      formData.append('policyTag', this.uploadForm.get('policyTag')?.value);
+      formData.append('docName', this.uploadForm.get('docName')?.value);
+      formData.append('uploadDoc', this.uploadForm.get('uploadDoc')?.value);
+      
+      this.http.post<any>('http://localhost:4000/cms/policyUploadPostAdmin', formData).subscribe(
+        (response: any) => {
+          console.log('Document uploaded successfully:', response);
+          // Handle the success response here if needed
+        },
+        (error: any) => {
+          console.error('Error uploading document:', error);
+          // Handle the error response here if needed
+        }
+      );
+    }
+  }
+
+  onFileChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files[0]) {
+      this.uploadForm.patchValue({
+        uploadDoc: inputElement.files[0],
+      });
+      this.uploadForm.get('uploadDoc')?.updateValueAndValidity();
+    }
+  }
 }
-}
+
